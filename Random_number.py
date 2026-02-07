6447e98ae2a712bdb19f75daf07abc7f501b96d1
Shnepst Sergey <sergey@shnepst.ru>
2026-02-07 18:24:18 +0300

    Добавлен выбор правой границы

--- Random_number.py
+++ Random_number.py
@@ -1,18 +1,29 @@
 import random
-
-
 # Блок функций
+# выбор числа для правой границы
+def choise_right_limit():
+    while True:
+        right_limit = input("Выберите в каком диапазоне загадать число компьютеру, от 1 до?\n")
+        if right_limit == "q":
+            exit()
+        elif right_limit.isdigit():
+            return int(right_limit)
+        else:
+            print('Можно ввести только целое число')
+
 # Проверка ввода числа от пользователя
 def is_valid(input_string):
-    return input_string.isdigit() and 1 <= int(input_string) <= 100
+    return input_string.isdigit() and 1 <= int(input_string) <= right_limit
 
 def input_num():
     while True:
         user_num = input()
-        if is_valid(user_num):
+        if user_num.lower() == 'q':
+            exit()
+        elif is_valid(user_num):
             return int(user_num)
         else:
-            print("А может быть все-таки введем целое число от 1 до 100?")
+            print(f"А может быть все-таки введем целое число от 1 до {right_limit}?")
 
 # Сравнение введенного пользователем числа и загадонного
 def compare_num():
@@ -27,55 +38,29 @@ def compare_num():
         else:
             return print(f"Вы угадали, поздравляем!\nКоличетсво попыток: {count}")
 
+# Предложение сыграть еще раз
+def play_again(): 
+    print("Хотите сыграть еще раз?\nОтвет \"y\" или \"n\"")
+    while True:
+        play_again = input()
+        if play_again == "q":
+            exit()
+        elif play_again.lower() == "y":
+            return True
+        elif play_again.lower() == "n":
+            print('Спасибо, запустите программу еще раз, если захотите поиграть в \"Угадайку\"')
+            break
+        else:
+            print('Можно ввести только \"y\" или \"n\" без кавычек')
 
 # Основаная программа
-num = random.randint(1, 101)
 print("Добро пожаловать в числовую угадайку\n")
-print("Введите число от 1 до 100 включительно")
-
-compare_num()
-print("Хотите сыграть еще раз?\nОтвет \"y\" или \"n\"")
-
+print("Компьютер загадает число, а вы должны его отгадать за минимальное количество попыток")
+print('Если захотите выйти введите \"q\" без кавычек')
 while True:
-    play_again = input()
-    if play_again == "y":
-        compare_num()
+    right_limit = choise_right_limit()
+    num = random.randint(1, right_limit + 1)
+    print(f"\nВведите число от 1 до {right_limit} включительно:")
+    compare_num()
+    if not play_again():
         break
-    elif play_again == "n":
-        print('Спасибо, запустите программу еще раз, если захотите поиграть в \"Угадайку\"')
-        break
-    else:
-        print('Можно ввести только \"y\" или \"n\" без кавычек')
-
-
-# count = 1
-
-
-#    count += 1
-
-
-# def random_number(fromm, to):
-#    num = random.randint(fromm, to)
-
-
-# пока не предумал как встроить через функцию
-# количество попыток для угадывания == 7
-def number_of_attempts():
-    middle = (left + right) // 2
-    count = 1
-    while middle != num:
-        if middle < num:
-            print("мало")
-            left = middle + 1
-            middle = (left + right) // 2
-        elif middle > num:
-            print("много")
-            right = middle - 1
-            middle = (left + right) // 2
-        num = int(input())
-        count += 1
-    print("Угадали", num)
-    print(count)
-
-
-# print(random_number(left, right))
