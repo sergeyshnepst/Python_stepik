36954b46ea70fb8295d9da5f15f38c6df8d72b30
Shnepst Sergey <sergey@shnepst.ru>
2026-02-11 20:47:19 +0300

    промежуточный этап Шифра Цезаря

---
+++ Caesar_cipher.py
@@ -0,0 +1,74 @@
+# x=(y−k) % n
+
+# где
+# x — символ открытого текста,
+# y — символ шифрованного текста,
+# n — мощность алфавита (количество символов), а
+# k — ключ.
+
+# Основная программа
+en_alphabet = "abcdefghijklmnopqrstuvwxyz"
+ru_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+n = 0
+new_text = ""
+
+# eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+
+# направление: шифрование или дешифрование
+while True:
+    direction = input("Вы ходите зашифровать или расшифровать текст?\n")
+    if direction.lower() == "расшифровать":
+        direction = "dec"
+        break
+    elif direction.lower() == "зашифровать":
+        direction = "enc"
+        break
+    else:
+        print("\nМожно ввести только 'расшифровать' или 'дешифровать' без кавычек")
+
+# язык алфавита: русский или английский
+while True:
+    language = input("На каком языке шифр?\n")
+    if language.lower() == "русский":
+        n = 32
+        language = ru_alphabet
+        break
+    elif language.lower() == "английский":
+        n = 26
+        language = en_alphabet
+        break
+    else:
+        print("Можно ввести только 'русский' или 'английский' без кавычек")
+
+# шаг сдвига (со сдвигом вправо)
+while True:
+    step = input("\nКакой шаг выбрать?\n")
+    if step.isdigit():
+        break
+    else:
+        print("Можно ввести только число")
+
+# Предложение ввести текст
+# TODO сделать выбор текста для шифровки и дешифровки
+text = input("\nВведите текст\n")
+
+# TODO добавить выбор шифровки/расшифровки
+# TODO добавить функцию
+for i in range(len(text)):
+    if text[i].isalpha():
+        if text[i] == text[i].upper():
+            for j in range(n):
+                if text[i] == language[j].upper():
+                    new_text += language[(j + int(step)) % n].upper()
+                    break
+        elif text[i] == text[i].lower():
+            for k in range(n):
+                if text[i] == language[k].lower():
+                    new_text += language[(k + int(step)) % n].lower()
+                    break
+    else:
+        new_text += text[i]
+
+print(new_text)
--- for_tests.py
+++ for_tests.py
@@ -1,102 +1,23 @@
-import random
+en_alphabet = "abcdefghijklmnopqrstuvwxyz"
+ru_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+n = 32
+new_text = ""
+step = "3"
 
 
-def generate_password(chars):
-    return random.choice(chars)
-
-
-# Блок основаной программы
-digits = "0123456789"
-lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-punctuation = "!#$%&*+-=?@^_"
-symbols = "il1Lo0O"
-chars = ""
-password = ""
-
-# Количество паролей для генерации;
-qty_pass = input("Сколько паролей нужно сгененировать?\n")
-while True:
-    if not qty_pass.isdigit():
-        print("Можно ввести только число")
-    else:
-        break
-
-# Длину одного пароля;
-len_pass = input("Какой длины (сколько символов) должен быть пароль?\n")
-while True:
-    if not len_pass.isdigit():
-        print("Можно ввести только число")
-    else:
-        break
-
-# Включать ли цифры 0123456789?
-while True:
-    answer = input(f"Включить цифры в пароль? 'да' или 'нет'\n")
-    if answer.lower() == "да":
-        chars += digits
-        break
-    elif answer.lower() == "нет":
-        break
-    else:
-        print("Можно ввести только 'да' или 'нет' без кавычек")
-
-# Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
-while True:
-    answer = input(f"Включить прописные буквы в пароль? 'да' или 'нет'\n")
-    if answer.lower() == "да":
-        chars += uppercase_letters
-        break
-    elif answer.lower() == "нет":
-        break
-    else:
-        print("Можно ввести только 'да' или 'нет' без кавычек")
-
-# Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
-while True:
-    answer = input(f"Включить строчные буквы в пароль? 'да' или 'нет'\n")
-    if answer.lower() == "да":
-        chars += lowercase_letters
-        break
-    elif answer.lower() == "нет":
-        break
-    else:
-        print("Можно ввести только 'да' или 'нет' без кавычек")
-
-# Включать ли символы !#$%&*+-=?@^_?
-while True:
-    answer = input(f"Включить спецсимволы в пароль? 'да' или 'нет'\n")
-    if answer.lower() == "да":
-        chars += punctuation
-        break
-    elif answer.lower() == "нет":
-        break
-    else:
-        print("Можно ввести только 'да' или 'нет' без кавычек")
-
-# Исключать ли неоднозначные символы il1Lo0O?
-
-# Продумать как исключить символы из уже известного набора
-result = ""
-while True:
-    answer = input("Исключить неоднознанчее символы 'il1Lo0O'?\n")
-    if answer.lower() == "да":
-        for i in symbols:
-            if i not in chars:
-                result += i
-        break
-    elif answer.lower() == "нет":
-        result = chars
-        break
-    else:
-        print("Можно ввести только 'да' или 'нет' без кавычек")
-
-
-# generate_password(result)
-
-
-# TODO
-# for i in range(qty_pass):
-
-for i in range(int(len_pass)):
-    password += generate_password(result)
+text = input("\nВведите текст\n")
+for i in range(len(text)):
+                if text[i].isalpha():
+                    if text[i] == text[i].upper():
+                        for j in range(n):
+                            if text[i] == ru_alphabet[j].upper():
+                                new_text += ru_alphabet[(j + int(step)) % n].upper()
+                                break
+                    elif text[i] == text[i].lower():
+                        for k in range(n):
+                            if text[i] == ru_alphabet[k].lower():
+                                new_text += ru_alphabet[(k + int(step)) % n].lower()
+                                break
+                else:
+                    new_text += text[i]
+print(new_text)
