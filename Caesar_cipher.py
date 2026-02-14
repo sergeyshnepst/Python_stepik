d8f21ac3c6058ef31235a0e814504028f457e279
Shnepst Sergey <sergey@shnepst.ru>
2026-02-14 20:21:00 +0300

    Добавление возможности дешифровки если не известен ключ

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -10,24 +10,27 @@
 en_alphabet = "abcdefghijklmnopqrstuvwxyz"
 ru_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 n = 0
+sign = ""
 
 def caesar_cipher():
-    new_text = ""
-    for i in range(len(text)):
-        if text[i].isalpha():
-            if text[i] == text[i].upper():
-                for j in range(n):
-                    if text[i] == language[j].upper():
-                        new_text += language[(j + int(step)) % n].upper()
-                        break
-            elif text[i] == text[i].lower():
-                for k in range(n):
-                    if text[i] == language[k].lower():
-                        new_text += language[(k + int(step)) % n].lower()
-                        break
-        else:
-            new_text += text[i]
-    return new_text
+    for x in range(start, qty_output + 1): # Вывод дешифровки в зав известен шаг или нет
+        new_text = ""
+        for i in range(len(text)):
+            if text[i].isalpha():
+                if text[i] == text[i].upper():
+                    for j in range(n):
+                        if text[i] == language[j].upper():
+                            new_text += language[(j + x) % n].upper()
+                            break
+                elif text[i] == text[i].lower():
+                    for k in range(n):
+                        if text[i] == language[k].lower():
+                            new_text += language[(k + x) % n].lower()
+                            break
+            else:
+                new_text += text[i]
+    #return new_text
+        print(new_text)
 
 # направление: шифрование или дешифрование
 while True:
@@ -43,7 +46,7 @@ while True:
 
 # язык алфавита: русский или английский
 while True:
-    language = input("На каком языке шифр?\n")
+    language = input("\nНа каком языке шифр?\n")
     if language.lower() == "русский" or language.lower() == "ru":
         n = 32
         language = ru_alphabet
@@ -56,20 +59,42 @@ while True:
         print("Можно ввести только 'русский' или 'английский' без кавычек")
 
 # шаг сдвига (со сдвигом вправо)
-while True:
-    step = input("\nКакой шаг выбрать?\n")
-    if step.isdigit():
-        break
-    else:
-        print("Можно ввести только число")
+if direction == "dec":
+    while True:
+        step = input("\nШаг известен?\n")
+        if step.lower() == "нет":
+            start = 1
+            qty_output = n
+            break
+        elif step.lower() == "да":
+            while True:
+                step = input("\nКакой шаг выбрать?\n")
+                if step.isdigit():
+                    start = -int(step)
+                    qty_output = -int(step)
+                    break
+                else:
+                    print("Можно ввести только число")
+            break
+        else:
+            print("Можно ввести только 'да' или 'нет'")
+elif direction == "enc":
+    while True:
+        step = input("\nКакой шаг выбрать?\n")
+        if step.isdigit():
+            start = int(step)
+            qty_output = int(step)
+            break
+        else:
+            print("Можно ввести только число")
 
 # Предложение ввести текст
 if direction == "dec":
-    step = - int(step) # подставновка знака "-" в формулу перед шагом
+    # step = -int(step)  # подставновка знака "-" в формулу перед шагом
     input_text = "расшифровки"
 elif direction == "enc":
     input_text = "шифровки"
 text = input(f"\nВведите текст для {input_text}\n")
 
 # Вывод результата
-print(caesar_cipher())
+caesar_cipher()
