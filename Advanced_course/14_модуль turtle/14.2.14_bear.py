ae0c72e32367214ebed9fdc6cb076f9734b0bb67
Shnepst Sergey <sergey@shnepst.ru>
2026-08-08 11:21:43 +0300

    bear

---
+++ Advanced_course/14_модуль turtle/14.2.14_bear.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+circle(200)
+circle(100)
+left(90)
+penup()
+forward(40)
+pendown()
+forward(100)
+right(90)
+circle(15)
+
+penup()
+goto(170, 330)
+pendown()
+circle(50)
+
+penup()
+goto(-170, 330)
+pendown()
+circle(50)
+    
+
+penup()
+goto(90, 190)
+pensize(15)
+dot()
+
+goto(-90, 190)
+pensize(15)
+dot()
+
+
+
+
+   
