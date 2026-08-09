0bda380ab669adcc0650c297207d190e73590fea
Shnepst Sergey <sergey@shnepst.ru>
2026-08-09 09:43:09 +0300

    halfmoon_animated

---
+++ Advanced_course/14_модуль turtle/14.3.7_halfmoon_animated.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+hideturtle()
+dot(200, 'gold')
+speed(1)
+penup()
+bgcolor('darkblue')
+goto(200, 0)
+showturtle()
+shape('circle')
+shapesize(10)
+color('darkblue')
+for _ in range(400):
+    backward(1)
+    
+mainloop()
+
+
+
+   
